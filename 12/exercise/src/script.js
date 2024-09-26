4068e840d0ef8977420957f0af70e03e24d2ec70
Bo <boprojectbo@gmail.com>
2024-09-26 18:04:36 +0200

    Final: 12 3D Text

--- 12/exercise/src/script.js
+++ 12/exercise/src/script.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
+
 
 /**
  * Base
@@ -14,20 +17,92 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// Axes helper
+const AxesHelper = new THREE.AxesHelper()
+scene.add(new THREE.AxesHelper)
+
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('/textures/matcaps/2.png')
+matcapTexture.colorSpace = THREE.SRGBColorSpace
+
+
+/**
+ * Fonts
+ */
+const fontLoader = new FontLoader()
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) =>
+    {
+        const textGeometry = new TextGeometry(
+            'Hello Bo Study',
+            {
+                font: font,
+                size: 0.5,
+                depth: 0.2,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 5
+            }
+        )
+        textGeometry.computeBoundingBox()
+        // textGeometry.translate(
+        //     - (textGeometry.boundingBox.max.x - 0.02) * 0.5, // Subtract bevel size
+        //     - (textGeometry.boundingBox.max.y - 0.02) * 0.5, // Subtract bevel size
+        //     - (textGeometry.boundingBox.max.z - 0.03) * 0.5  // Subtract bevel thickness
+        // )
+        textGeometry.center()
+
+
+        //const textMaterial = new THREE.MeshBasicMaterial({wireframe : false})
+        
+        // const textMaterial = new THREE.MeshMatcapMaterial({matcap: matcapTexture})
+        const material = new THREE.MeshMatcapMaterial({matcap: matcapTexture})
+        // textMaterial.wireframe = false
+        const text = new THREE.Mesh(textGeometry, material)
+        scene.add(text)
+
+        console.time('donuts')
+
+        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+        const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+
+        for(let i = 0; i < 300; i++)
+            {
+               
+                const donut = new THREE.Mesh(donutGeometry, material)
+                donut.position.x = (Math.random() - 0.5) * 10
+                donut.position.y = (Math.random() - 0.5) * 10
+                donut.position.z = (Math.random() - 0.5) * 10
+
+                donut.rotation.x = Math.random() * Math.PI
+                donut.rotation.y = Math.random() * Math.PI
+
+                const scale = Math.random()
+                donut.scale.set(scale , scale , scale)
+
+                scene.add(donut)
+            }
+            console.timeEnd('donuts')
+    }
+)
+
 
 /**
  * Object
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial()
+// )
 
-scene.add(cube)
+// scene.add(cube)
 
 /**
  * Sizes
